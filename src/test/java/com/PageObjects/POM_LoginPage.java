eda6c439a3b745f4eb78c4d3c762565725a0a9d0
Amreen <amreennazia1989@gmail.com>
2024-04-24 22:41:02 -0500

    Restore

--- src/test/java/com/PageObjects/POM_LoginPage.java
+++ src/test/java/com/PageObjects/POM_LoginPage.java
@@ -1,6 +1,11 @@
 package com.PageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.Utilities.Constant;
+import com.Utilities.ElementsUtil;
 
 public class POM_LoginPage {
 	
@@ -11,8 +16,29 @@ public class POM_LoginPage {
 		this.driver = driver;
 	}
 	
-//	public void getHomepage() {
-//		
-//	}
+	@FindBy(xpath = "//input[@id='username']")
+	WebElement txt_user;
+	@FindBy(xpath ="//input[@id='password']")
+	WebElement txt_password;
+	@FindBy(xpath ="//button[@id='login']")
+	WebElement btn_login;
+	
+
+	public String getLoginPage() {
+		return driver.getTitle();
+	}
+
+	 public void enterValidCredentials(String validUserName,String validPwd) {
+		 ElementsUtil.typeInputIntoElement(driver, txt_user,validUserName, Constant.EXPLICIT_ELEMENT_WAIT_TIME);
+		 ElementsUtil.typeInputIntoElement(driver, txt_password,validPwd, Constant.EXPLICIT_ELEMENT_WAIT_TIME);
+		 
+	 }
+	public void login() {
+		
+		ElementsUtil.ScrolltoElementandClick(driver, btn_login, Constant.EXPLICIT_ELEMENT_WAIT_TIME);
+	}
+	public String getHomePageTitle() {
+		return driver.getTitle();// DashBoard Page
+	}
 
 }
