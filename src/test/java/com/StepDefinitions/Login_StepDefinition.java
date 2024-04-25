d97e5a25537da48b5cc45f1599f484eaca7d6173
Amreen <amreennazia1989@gmail.com>
2024-04-24 22:53:04 -0500

    a

--- src/test/java/com/StepDefinitions/Login_StepDefinition.java
+++ src/test/java/com/StepDefinitions/Login_StepDefinition.java
@@ -1,7 +1,15 @@
 package com.StepDefinitions;
 
+import org.testng.Assert;
+
 import com.PageObjects.POM_LoginPage;
+import com.Utilities.Constant;
+import com.Utilities.LoggerLoad;
 import com.Utilities.TestContext;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class Login_StepDefinition {
 	
@@ -11,8 +19,28 @@ public class Login_StepDefinition {
 	public Login_StepDefinition(TestContext testcontext) {
 		
 		this.testcontext = testcontext;
-		loginpage = testcontext.getpageObjectManager().getLoginPage();
+		loginpage = testcontext.pageobjectmanager.getPage_01();
 	}
 	 
+	@Given("User is already on Login page")
+	public void user_is_already_on_login_page() {
+	      Assert.assertEquals(loginpage.getLoginPage(), Constant.HOME_PAGE_TITLE);
+	}
 
+	@When("User enters valid {string} and valid {string}")
+	public void user_enters_valid_and_valid(String user, String password) {
+	     loginpage.enterValidCredentials(user, password);
+	}
+
+	@When("User clicks on login button")
+	public void user_clicks_on_login_button() {
+	     loginpage.login();
+	}
+
+	@Then("User should be redirected to DashBoard Page")
+	public void user_should_be_redirected_to_dash_board_page() {
+		String pageTitle =	loginpage.getHomePageTitle();
+		LoggerLoad.info("Home Page title is :-" + pageTitle);
+		Assert.assertEquals(loginpage.getHomePageTitle(),Constant.HOME_PAGE_TITLE );
+	}
 }
