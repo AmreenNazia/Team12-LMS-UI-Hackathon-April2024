d5d04e8275addf68a36018b7115368434aa3e6a2
anusha <anushareddy.59@gmail.com>
2024-04-29 21:29:50 -0700

    Fixed compilation errors

--- src/test/java/com/Utilities/pageObjectManager.java
+++ src/test/java/com/Utilities/pageObjectManager.java
@@ -3,39 +3,33 @@ package com.Utilities;
 import org.openqa.selenium.WebDriver;
 
 import com.PageObjects.POM_LoginPage;
-import com.PageObjects.POM_UserEditPage;
-import com.PageObjects.POM_UserPage;
 
 public class pageObjectManager {
-
+	
 	WebDriver driver;
-	POM_LoginPage loginpage;
-	POM_UserPage userpage;
-	POM_UserEditPage userEditPage;
-	public pageObjectManager(WebDriver driver) {
+	 
+	
+	public pageObjectManager(WebDriver driver)
+	{
 		this.driver = driver;
 	}
-
+	
 	public POM_LoginPage getLoginPage() {
-		if (loginpage == null) {
+		if(loginpage==null)
+		{
 			loginpage = new POM_LoginPage(driver);
 		}
 		return loginpage;
-
+		
 	}
-
-	public POM_UserPage getuser() {
-		if (userpage == null) {
-			userpage = new POM_UserPage(driver);
+public POM_UserPage getuser() {
+			if(userpage==null)
+			{
+				userpage = new POM_UserPage(driver);
+			}
+			return userpage;
 		}
-		return userpage;
-	}
 
-	public POM_UserEditPage getUserEditPage() {
-		if(userEditPage == null) {
-			userEditPage = new POM_UserEditPage(driver);
-		}
-		return userEditPage;
-	}
+
 
 }
